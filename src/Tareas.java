6f103114bec0caa6db53e9a6698db2a2e9532602
Jesjimrod <jesjimrod@alu.edu.gva.es>
2026-01-15 12:56:40 +0100

    Clase Tareas creada con dos métodos

---
+++ src/Tareas.java
@@ -0,0 +1,9 @@
+public class Tareas {
+
+    public void printList() {
+        System.out.println("Lista:\n-Comprar\n-Pasear");
+    }
+    public  void agregarTarea(String tarea) {
+        System.out.println("Tarea añadida " + tarea);
+    }
+}
